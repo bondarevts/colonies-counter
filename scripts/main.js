87ff4adfcf6542935a41b21b85e69c90c69062ef
Timofei Bondarev <bondarevts@gmail.com>
2020-06-20 17:06:26 -0400

    Add a button to download all points. Store the file name of an uploaded image.

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ var diameter = 50;
 var canvas = SVG('canvas-wrapper').size(400, 100).style('touch-action', 'manipulation');
 var text = canvas.text("Load an image");
 
+var file_name = "";
 var marks = [];
 var points = [];
 
@@ -52,11 +53,13 @@ function drawImage(img) {
 }
 
 function loadTestImage() {
-    drawImage("imgs/test_image.jpg");
+    file_name = "test_image.jpg"
+    drawImage("imgs/" + file_name);
 }
 
 function loadImage() {
     var file = document.getElementById('image-browser').files[0];
+    file_name = file.name;
     var reader = new FileReader();
     reader.onload = function() {
         drawImage(reader.result);
@@ -67,4 +70,23 @@ function loadImage() {
 function drawCircle(x, y, diameter) {
     var radius = diameter / 2;
     return canvas.circle(diameter).fill("rgba(0, 0, 255, 0.4)").move(x - radius, y - radius);
+}
+
+function pointsToCSV() {
+    return points.map(p => p.join(",")).join("\n");
+}
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+function downloadPoints() {
+    download(file_name + ".csv", pointsToCSV())
 }
